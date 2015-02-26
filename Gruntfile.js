49c1ea7f3301d46edf7a5045dd3bfba5548b8c67
Scott Kingsley Clark <lol@scottkclark.com>
2015-02-25 20:21:58 -0600

    Clean up zip release file

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,7 @@ module.exports = function (grunt) {
 		clean: {
 			post_build: [
 				'.build',
+				'release/<%= pkg.name %>-<%= pkg.version %>.zip',
 				'release/<%= pkg.name %>',
 				'release/build/<%= pkg.version %>',
 				'release/build/'
